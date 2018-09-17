96e78085abbe81cbebab0c93457e6b238c27db99
Mads Vainoe Baatrup <mvb@teknologisk.dk>
2018-09-17 09:25:38 +0200

    removed bins

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mvb/Projects/dkr_img_tst/src
-# Build directory: /home/mvb/Projects/dkr_img_tst/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
-subdirs(dkr_img_tst)
--- build/dkr_img_tst/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mvb/Projects/dkr_img_tst/src/dkr_img_tst
-# Build directory: /home/mvb/Projects/dkr_img_tst/build/dkr_img_tst
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
